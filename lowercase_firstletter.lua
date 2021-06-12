9cd5d400f35534c21156650f2e0c15d3a03c88b0
Clément DELALANDE <delalande.clem@gmail.com>
2021-06-13 00:51:34 +0200

    feat: add string.lower to selected lines
    This script isn't finished yet, since I need to check
    the first and second "letter" to do the change

--- lowercase_firstletter.lua
+++ lowercase_firstletter.lua
@@ -6,10 +6,13 @@ script_author = "Clément 'iKlem' Delalande"
 script_version = "0.1.0"
 
 function toLowerCase(subtitles, selected_lines, active_line)
-	aegisub.debug.out("Hello World")
-	aegisub.debug.out(subtitles.n .. " and " .. #subtitles .. " should be the same value")
-	aegisub.debug.out(subtitles[selected_lines[1]].raw)
-	aegisub.debug.out(subtitles[selected_lines[1]].class)
+	for i=1, #selected_lines do
+		local subLine = subtitles[selected_lines[i]]
+		subLine.text = string.lower(subLine.text)
+		subtitles[selected_lines[i]] = subLine
+	end
+
+	aegisub.set_undo_point(script_name)
 end
 
 aegisub.register_macro(script_name, script_description, toLowerCase)
