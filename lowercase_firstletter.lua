f8a49afe86a35fed52113b0987c447e398c702b0
Clément DELALANDE <delalande.clem@gmail.com>
2021-06-13 01:59:10 +0200

    fix: changed pattern
    The pattern will be explained in the next commit, in the
    readme.md file

--- lowercase_firstletter.lua
+++ lowercase_firstletter.lua
@@ -1,15 +1,30 @@
 local tr = aegisub.gettext
 
-script_name = tr"lowercase_firstletter"
+script_name = tr"To lower case the first letter"
 script_description = tr"Put to lower case the first letter of lines (since there's no upper case in japanese)"
 script_author = "Clément 'iKlem' Delalande"
-script_version = "0.1.0"
+script_version = "0.3.0"
 
 function toLowerCase(subtitles, selected_lines, active_line)
+	local patternToLower = "({.*})(%u)( ?[%l]?)([^%u].*)"
+	local text, tag, upper, lower
+	local subLine
+
 	for i=1, #selected_lines do
-		local subLine = subtitles[selected_lines[i]]
-		subLine.text = string.lower(subLine.text)
-		subtitles[selected_lines[i]] = subLine
+		subLine = subtitles[selected_lines[i]]
+
+		text = ""
+		for tag, upper, lower, remainder in subLine.text:gmatch( patternToLower ) do
+			if lower ~= nil then
+					upper = string.lower( upper )
+					text = tag .. upper .. lower .. remainder
+			end
+		end
+
+		if text ~= "" then
+			subLine.text = text
+			subtitles[selected_lines[i]] = subLine
+		end
 	end
 
 	aegisub.set_undo_point(script_name)
